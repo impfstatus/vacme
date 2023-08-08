dcc7e90a75adc4c191042420726e60ac66832eab
impfstatus <impfstatus@gmail.com>
2023-08-08 17:16:48 +0200

    first commit

--- macros.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-MinTime = datetime.datetime(1970, 1, 1, 1, 0)
-MaxTime = datetime.datetime.fromtimestamp(2147483647)
--- vacmelogger.py
+++
@@ -1,8 +0,0 @@
-from logging.config import fileConfig
-import logging
-import sys
-import config
-
-fileConfig('logging_config.ini')
-logger = logging.getLogger("vacme")
-
